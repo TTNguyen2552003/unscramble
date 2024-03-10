1c8791bc39e949fd0313f99e5b51d135bd755931
Nguyen Tran Trong <121627783+TTNguyen2552003@users.noreply.github.com>
2024-03-10 11:11:36 +0000

    fix: Update API functionality for data

--- app/src/main/java/app/kotlin/unscramble/data/AppContainer.kt
+++ app/src/main/java/app/kotlin/unscramble/data/AppContainer.kt
@@ -14,7 +14,7 @@ interface AppContainer {
 
 class DefaultAppContainer(context: Context) : AppContainer {
     private val baseUrl =
-        "https://firebasestorage.googleapis.com/v0/b/chat-app-4db61.appspot.com/o/data.json?alt=media&token=8d55a584-90d9-4795-b6ab-3695fd84b9b9"
+        "https://firebasestorage.googleapis.com/v0/b/unscramble-storage.appspot.com/o/data.json?alt=media&token=29a1769e-6464-4d6e-994f-e7f27b78971c"
 
     private val httpClient: HttpClient = HttpClient(Android) {
         install(plugin = ContentNegotiation) {
